00f54a2ed1203d319a54fa24570fda748dfc0457
Estevan Jantsk <estevan.jantsk@gmail.com>
2023-03-26 23:45:23 +0300

    add course.vue

---
+++ pages/course.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col"
+    >
+    <div
+      class="prose mb-12"
+    >
+      <h1 class="text-3xl text-center">
+        <span class="font-medium">
+          <span class="font-bold">Course Title</span>
+        </span>
+      </h1>
+    </div>
+
+    <div class="flex flex-row justify-center flex-grow">
+      <div
+        class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col"
+      >
+        <h3>Chapters</h3>
+      </div>
+
+      <div class="prose p-12 bg-white rounded-md w-[65ch]">
+        <h2>Lesson</h2>
+        <p>This is a lesson</p>
+      </div>
+    </div>
+  </div>
+</template>
